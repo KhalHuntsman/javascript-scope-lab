a553cc402b876855740ff03fd9b8a398e48f7bbc
khal_huntsman <hunter.jude.steele@gmail.com>
2025-10-17 01:09:35 -0500

    Made edits to the index.js file as per instructions for the lab.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your solution in this file!
+
+const burgers = ["Hamburger", "Cheeseburger"]; // define the burgers array
+let featuredDrink = "Strawberry Milkshake"; // define the featured drink variable
+
+function addBurger() { // add a burger functionality
+  const newBurger = "Flatburger"; // uses const to define 'newBurger'
+  burgers.push(newBurger);
+  if(true) {
+    const anotherNewBurger = "Maple Bacon Burger";
+    burgers.push(anotherNewBurger)
+  }
+}
+
+function changeFeaturedDrink () {
+    featuredDrink = "The JavaShake"; // change the featured drink name
+}
